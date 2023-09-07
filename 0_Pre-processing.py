5d9bd07afb13c3eb26003dc79f02363c3bd7c942
Otniel Gomes <139892125+OtnielGomes@users.noreply.github.com>
2023-09-07 06:06:54 -0300

    2023-09-07

---
+++ 0_Pre-processing.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import pickle
+# Loading
+census_data = pd.read_csv('census.csv')
+# Separating classifiers
+X_census = census_data.iloc[:, 0:14].values
+y_census = census_data.iloc[:, 14].values
+pd.options.display.max_columns = None
+# Label Encoder
+label_encoder_work_class = LabelEncoder()
+label_encoder_education = LabelEncoder()
+label_encoder_marital_status = LabelEncoder()
+label_encoder_occupation = LabelEncoder()
+label_encoder_relationship = LabelEncoder()
+label_encoder_race = LabelEncoder()
+label_encoder_sex = LabelEncoder()
+label_encoder_native_country = LabelEncoder()
+
+X_census[:, 1] = label_encoder_work_class.fit_transform(X_census[:, 1])
+X_census[:, 3] = label_encoder_education.fit_transform(X_census[:, 3])
+X_census[:, 5] = label_encoder_marital_status.fit_transform(X_census[:, 5])
+X_census[:, 6] = label_encoder_occupation.fit_transform(X_census[:, 6])
+X_census[:, 7] = label_encoder_relationship.fit_transform(X_census[:, 7])
+X_census[:, 8] = label_encoder_race.fit_transform(X_census[:, 8])
+X_census[:, 9] = label_encoder_sex.fit_transform(X_census[:, 9])
+X_census[:, 13] = label_encoder_native_country.fit_transform(X_census[:, 13])
+
+scaler_census = StandardScaler()
+X_census = scaler_census.fit_transform(X_census)
+
+with open('census_date.pkl', mode='wb') as f:
+    pickle.dump([X_census, y_census], f)
+
+
+
+
+
+
---
+++ 1_PCA.py
@@ -0,0 +1,87 @@
+import pickle
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from yellowbrick.classifier import ConfusionMatrix
+
+# Pre-processing
+with open('census_date.pkl', mode='rb') as f:
+    X_census, y_census = pickle.load(f)
+# Reducing the classifiers
+# PCA
+pca = PCA(n_components=8)
+X_census_pca = pca.fit_transform(X_census)
+# Kernel PCA
+kernel_pca = KernelPCA(n_components=8, kernel='rbf')
+X_census_kernel_pca = kernel_pca.fit_transform(X_census)
+# LDA
+lda = LinearDiscriminantAnalysis(n_components=1)
+X_census_lda = lda.fit_transform(X_census, y_census)
+
+# Training
+# PCA
+neural_network_pca = MLPClassifier(hidden_layer_sizes=(5, 5),
+                                   activation='relu',
+                                   solver='adam',
+                                   batch_size=128,
+                                   max_iter=3000,
+                                   tol=1e-5,
+                                   verbose=True)
+neural_network_pca.fit(X_census_pca, y_census)
+
+# Prediction
+prediction_pca = neural_network_pca.predict(X_census_pca)
+prediction_pca_accuracy = accuracy_score(y_census, prediction_pca)
+# KernelPCA
+neural_network_kernel_pca = MLPClassifier(hidden_layer_sizes=(5, 5),
+                                          activation='relu',
+                                          solver='adam',
+                                          batch_size=128,
+                                          max_iter=3000,
+                                          tol=1e-5,
+                                          verbose=True)
+neural_network_kernel_pca.fit(X_census_kernel_pca, y_census)
+
+# Prediction
+prediction_kernel_pca = neural_network_kernel_pca.predict(X_census_kernel_pca)
+prediction_kernel_pca_accuracy = accuracy_score(y_census, prediction_kernel_pca)
+# LDA
+neural_network_lda = MLPClassifier(hidden_layer_sizes=(1, 1),
+                                   activation='relu',
+                                   solver='adam',
+                                   batch_size=128,
+                                   max_iter=3000,
+                                   tol=1e-5,
+                                   verbose=True)
+neural_network_lda.fit(X_census_lda, y_census)
+
+# Prediction
+prediction_lda = neural_network_lda.predict(X_census_lda)
+prediction_lda_accuracy = accuracy_score(y_census, prediction_lda)
+
+# Pos-processing
+# PCA
+print(f'\nAccuracy PCA: {prediction_pca_accuracy}'
+      f'\nClassification Report PCA: '
+      f'\n{classification_report(y_census, prediction_pca)}')
+cm = ConfusionMatrix(neural_network_pca)
+cm.fit(X_census_pca, y_census)
+cm.score(X_census_pca, y_census)
+cm.show()
+# KernelPCA
+print(f'\nAccuracy KernelPCA: {prediction_kernel_pca_accuracy}'
+      f'\nClassification Report KernelPCA: '
+      f'\n{classification_report(y_census, prediction_kernel_pca)}')
+cm = ConfusionMatrix(neural_network_kernel_pca)
+cm.fit(X_census_kernel_pca, y_census)
+cm.score(X_census_kernel_pca, y_census)
+cm.show()
+# LDA
+print(f'\nAccuracy LDA: {prediction_lda_accuracy}'
+      f'\nClassification Report LDA: '
+      f'\n{classification_report(y_census, prediction_lda)}')
+cm = ConfusionMatrix(neural_network_lda)
+cm.fit(X_census_lda, y_census)
+cm.score(X_census_lda, y_census)
+cm.show()
